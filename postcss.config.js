8b3aaeb903cb9eca8258a10a2bfe1154aed9f63f
mohammad ali baymani <buymoney.10@gmail.com>
2025-11-13 19:00:16 +0330

    fix tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 // postcss.config.js
 module.exports = {
   plugins: {
-    '@tailwindcss/postcss': {},
+    '@tailwindcss/postcss': {}, // <-- استفاده از پلاگین جدید
     autoprefixer: {},
   },
 }
